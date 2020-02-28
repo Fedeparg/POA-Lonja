762b3e8bfde0e172180ffa2701755e2164feffc5
Mery <meryguillen10@gmail.com>
2020-02-28 13:52:37 +0100

    Intento de admision de vendedor

--- LonjaPOA/src/Ontologia/Articulo.java
+++ LonjaPOA/src/Ontologia/Articulo.java
@@ -1,5 +1,17 @@
 package Ontologia;
 
+import java.util.Date;
+
 public class Articulo {
 
+	//private int id;
+	private Pescado pescado;
+	private double kilos;
+	private Vendedor vendedor;
+	private double precioSalida;
+	private double precioReserva;
+	private double precioFinal;
+	private Date horaRegistro;
+	private Date horaVenta;
+	
 }
