bea709f905b732402bdb57b72797c639b487befe
Mery <meryguillen10@gmail.com>
2020-03-11 13:58:52 +0100

    Ahora añadiendo las clases nuevas que he creado porque soy torpe :))

---
+++ LonjaPOA/src/Protocolos/DepositoArticuloP.java
@@ -0,0 +1,42 @@
+package Protocolos;
+
+import Agentes.AgenteLonja;
+import Ontologia.Articulo;
+import jade.core.Agent;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.lang.acl.UnreadableException;
+import jade.proto.AchieveREResponder;
+
+@SuppressWarnings("serial")
+public class DepositoArticuloP extends AchieveREResponder{
+
+	public DepositoArticuloP(Agent a, MessageTemplate mt) {
+		super(a, mt);
+	}
+	
+	protected ACLMessage prepareResponse (ACLMessage msjDeposito) {
+		System.out.println(myAgent.getLocalName() + ": recibido mensaje de deposito de articulo");
+		Articulo articulo = null;
+		try {
+			articulo = (Articulo) msjDeposito.getContentObject();
+		} catch (UnreadableException e) {
+			System.out.println(myAgent.getLocalName() + ": fallo al extraer el articulo del mensaje");
+			e.printStackTrace();
+		}
+		ACLMessage msjRespuesta = msjDeposito.createReply();
+		if (articulo != null && ((AgenteLonja) myAgent).containsVendedor(msjDeposito.getSender())) {
+			((AgenteLonja) myAgent).addArticuloParaSubastar(articulo);
+			System.out.println(myAgent.getLocalName() + ": Depositado articulo " + articulo.getID());
+			msjRespuesta.setPerformative(ACLMessage.INFORM);
+			msjRespuesta.setContent("Articulo depositado con exito");
+		} else {
+			msjRespuesta.setPerformative(ACLMessage.FAILURE);
+			msjRespuesta.setContent("Fallo al depositar el articulo");
+		}
+		return msjRespuesta;
+
+	}
+
+	
+}
